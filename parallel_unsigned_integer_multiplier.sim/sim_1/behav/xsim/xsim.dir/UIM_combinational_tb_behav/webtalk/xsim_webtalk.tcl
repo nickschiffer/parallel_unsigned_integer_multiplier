f21e76d9d56b16aa3c45fcb3e7ae413565652879
nickschiffer <nick@schiffer.us>
2018-03-16 19:29:36 -0700

    starting work on pipeline implementation

--- parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/xsim_webtalk.tcl
+++ parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Nick Schiffer/Documents/School/CMPE125/Labs/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Mar 10 22:27:03 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Mar 13 14:55:01 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2017.4 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2086221" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "c6e3b2c8-7c62-42eb-80ee-fddb30cd7f05" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "8279df5f6b254a2790f13682a0bed2ec" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "12" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "18" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-6300HQ CPU @ 2.30GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1280 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "8196_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2052046538 -regid "" -xml C:/Users/Nick Schiffer/Documents/School/CMPE125/Labs/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Nick Schiffer/Documents/School/CMPE125/Labs/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Nick Schiffer/Documents/School/CMPE125/Labs/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8160_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1244039484 -regid "" -xml C:/Users/Nick Schiffer/Documents/School/CMPE125/Labs/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Nick Schiffer/Documents/School/CMPE125/Labs/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Nick Schiffer/Documents/School/CMPE125/Labs/parallel_unsigned_integer_multiplier/parallel_unsigned_integer_multiplier.sim/sim_1/behav/xsim/xsim.dir/UIM_combinational_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
